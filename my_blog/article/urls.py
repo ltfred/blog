2b4492f7132d4f77df944706665d9ad7d33aab4e
fred <liutao@Freddembp.local>
2019-09-16 12:32:22 +0800

    markdown

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from article import views
 
 urlpatterns = [
+    # 所有文章
     url(r'^list/$', views.ArticleListView.as_view(), name='article_list'),
+    # 文章详情
+    url(r'^detail/(?P<article_id>\d+)/$', views.ArticleDetailView.as_view(), name='article_detail')
 ]
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -1,3 +1,4 @@
+import markdown
 from django import http
 from django.shortcuts import render
 
@@ -21,3 +22,27 @@ class ArticleListView(View):
         context = {'articles': articles}
 
         return render(request, 'article/list.html', context)
+
+
+class ArticleDetailView(View):
+    """文章详情"""
+
+    def get(self, request, article_id):
+
+        try:
+            article = ArticlePost.objects.get(id=article_id)
+        except:
+            return http.HttpResponse('数据库错误')
+
+        # 将markdown语法渲染成html样式
+        article.body = markdown.markdown(article.body,
+                                         extensions=[
+                                             # 包含 缩写、表格等常用扩展
+                                             'markdown.extensions.extra',
+                                             # 语法高亮扩展
+                                             'markdown.extensions.codehilite',
+                                         ])
+
+        context = {'article': article}
+
+        return render(request, 'article/detail.html', context)
